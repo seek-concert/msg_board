c444feede5da2f2b570d12540f04999503f7815a
seek-concert <1105420247@qq.com>
2019-11-04 18:07:55 +0800

    框框

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,10 +1,38 @@
 <?php
 namespace app\index\controller;
-
-class Index
+use think\Controller;
+/**
+ * 首页
+ * Class Index
+ * @package app\index\controller
+ */
+class Index extends Controller
 {
+    /**
+     * 初始化操作
+     * @access protected
+     */
+    public function _initialize()
+    {
+        parent::_initialize();
+    }
+
+    /**
+     * 首页视图
+     * @return \think\response\View
+     */
     public function index()
     {
         return view();
+    }
+
+    /**
+     * 留言
+     * @param array $data
+     * @return \think\response\View
+     */
+    public function msg($data=[])
+    {
+        return renderJson('0','留言成功','');
     }
 }
